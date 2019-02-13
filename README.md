343106d87753acc496f10ab6e63841c9245aa24c
Qiang Li <liqiang@gmail.com>
2019-02-12 18:12:34 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#aws
+
+```
+wget -O - https://raw.githubusercontent.com/asperitus/aws/master/lightsail/install-docker.sh | bash -s
+```
+
+```
+curl https://raw.githubusercontent.com/asperitus/aws/master/lightsail/install-docker.sh | bash -s
+```
+
---
+++ lightsail/install-docker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#test in docker 
+#docker run --rm -i -t  -v $PWD:/app/ --name test ubuntu:bionic /bin/bash
+
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update -y
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo usermod -aG docker ${USER}
+docker --version
+
+#
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+##
+
+
+
